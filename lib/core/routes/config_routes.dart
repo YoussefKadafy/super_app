400184b4e335c473887f5c17ae7c80ccb079eafe
YoussefKadafy <kadafyyoussef@gmail.com>
2026-01-13 01:00:27 +0200

    refactor initial route and fix file name of splash to onboarding screen and move it to  onboarding feature

--- lib/core/routes/config_routes.dart
+++ lib/core/routes/config_routes.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:super_app/core/routes/app_routes.dart';
-import 'package:super_app/splash_screen.dart';
+import 'package:super_app/features/onboarding/presentation/screens/onboarding_screen.dart';
 
 class ConfigRoutes {
   static final GoRouter routerConfig = GoRouter(
-    initialLocation: AppRoutes.splash,
+    initialLocation: AppRoutes.onboarding,
     routes: [
       GoRoute(
-        path: AppRoutes.splash,
-        name: AppRoutes.splash,
+        path: AppRoutes.onboarding,
+        name: AppRoutes.onboarding,
         builder: (context, state) {
           return const OnboardingTextAnimation();
         },
